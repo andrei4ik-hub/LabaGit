91cb41c83653de7ddf424bad2ffc338cbf9b54f2
andrew <andrey_budnyak@mail.ru>
2023-01-19 23:47:16 +0300

    Реализация функции-заглушки

---
+++ Main-project/Process.cpp
@@ -0,0 +1,24 @@
+#include "filter.h"
+#include <cstring>
+#include <iostream>
+
+rainfall_sub** GGfilter(rainfall_sub* array[], int size, bool (*check)(rainfall_sub* element), int& result_size)
+{
+	rainfall_sub** result = new rainfall_sub * [size];
+	result_size = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (check(array[i]))
+		{
+			result[result_size++] = array[i];
+		}
+	}
+	return result;
+}
+
+bool check_rainfall_sub_by_rain(rainfall_sub* element)
+{
+
+	return strstr(element->OurType.type, "rain") == 0;
+}
+
---
+++ Main-project/Processing.h
@@ -0,0 +1,8 @@
+#ifndef PROCESSING_H
+#define PROCESSING_H
+
+#include "book_subscription.h"
+
+int process(book_subscription* array[], int size);
+
+#endif
